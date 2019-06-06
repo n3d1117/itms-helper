b13061bd0d1d6faf772e2d7d8bd764b4607ee8dd
ned <ned@github>
2019-06-06 20:54:27 +0200

    initial commit

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "itms-helper",
+    dependencies: [
+        // 💧 A server-side Swift web framework.
+        .package(url: "https://github.com/vapor/vapor.git", .upToNextMinor(from: "3.3.0")),
+    ],
+    targets: [
+        .target(name: "App", dependencies: ["Vapor"]),
+        .target(name: "Run", dependencies: ["App"]),
+        .testTarget(name: "AppTests", dependencies: ["App"]),
+    ]
+)
+
